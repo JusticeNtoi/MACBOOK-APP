e916f3192d25e17fecb875ddfe74daea864a1f99
Justice Ntoi <korotsoane.ntoi@juligerm.com>
2025-11-26 14:28:45 +0200

    first commit

---
+++ components/NavBar/index.tsx
@@ -0,0 +1,31 @@
+import { navLinks } from "@/constants";
+import Image from "next/image";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <Image src="/logo.svg" alt="Apple logo" width={24} height={24} />
+
+        <ul>
+          {navLinks.map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex-center gap-3">
+          <button>
+            <Image src="/search.svg" alt="Search" width={24} height={24} />
+          </button>
+          <button>
+            <Image src="/cart.svg" alt="Cart" width={24} height={24} />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
