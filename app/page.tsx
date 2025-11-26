03bc27abc9cfcdd5b0882e57a1ace2ccd58a4de3
Justice Ntoi <korotsoane.ntoi@juligerm.com>
2025-11-26 18:05:25 +0200

    3D Product Viewer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import Hero from "@/components/Hero";
-import NavBar from "@/components/NavBar";
+import Hero from "@/components/hero";
+import NavBar from "@/components/navBar";
+import ProductViewer from "@/components/productViewer";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 }
